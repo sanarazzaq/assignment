1cb3d51498aebcacdd3a08ad6405b6ef0cfac5ed
sanarazzaq <73792832+sanarazzaq@users.noreply.github.com>
2021-02-16 22:02:37 +0500

    Add files via upload

---
+++ declration.java
@@ -0,0 +1,14 @@
+public class declration{ 
+ public static void main(string[] args){
+    int x=3;
+    int y=2;
+    system.out.println(x);
+    system.out.println(y);
+      x=y;
+    system.out.println(x);
+    system.out.println(y);
+     y=5;
+    system.out.println(x);
+    system.out.println(y);
+ }
+}
